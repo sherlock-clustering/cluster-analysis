e04e16ba85bdfde1b3ce7c32a37fed24ef24faa4
Sonja Georgievska <s.georgievska@esciencecenter.nl>
2017-05-16 15:54:44 +0200

    cleaning datasets

--- scripts/create_db.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE GRAPH (
-   node1 TEXT,
-   node2 TEXT,
-   weight NUMERIC,
-   PRIMARY KEY(node1, node2)
-);
-
---.sep \t
---.import FILENAME GRAPH
--- scripts_Python3/netclust.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-score=0
-for file in $@; do
-   echo Processing $file...
-   netindex $file && netclust $file F1 S $score
-done;
-
